3d7d8230620b12969d858ae1f4fa596b9763ec0e
helensereda <sereda.elenaby@gmail.com>
2023-04-30 11:28:59 +0300

    upd2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,41 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+       import java. util.Scanner; // импорт библиотеки
+        public class Main
+        {
+            public static void main(String[] args)
+            {
+// моя первая программа
+                System.out.println(""); // пустая строка
+                System.out.println("Hello my friend");
 
 
+                System.out.print("Please, go home");
+                System.out.println ();
+                System.out.print("Please, read Роберт Мартин");
+                System.out.println("");
+                System.out.print("Please, learn");
+                System.out.println("");
+                System.out.println("Let's calculate sum of 5 + 10:" + 5 + 10);
+                System.out.println("Let's calculate sum of 5 + 10:" + (5 + 10));
 
-    }
-}
+                System.out.println ("Helen" + " " + "Sereda" +
+                        ". Welcom" +
+                        ". Lesson to....");
+
+                System.out.println("");
+
+                System.out.println("Please, enter your name and surname: ");
+                Scanner scanner = new Scanner(System.in);
+                scanner. nextLine();
+
+
+                String name = "123";
+                int age = 124;
+                double weight = 88.1;
+                char symbol = 'A';
+            }
+        }
+
+
