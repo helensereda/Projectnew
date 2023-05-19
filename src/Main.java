54d4cf9575392b186f096ae82f6c528e4d774a8f
helensereda <sereda.elenaby@gmail.com>
2023-05-19 12:02:14 +0300

    arrays change

--- src/Main.java
+++ src/Main.java
@@ -5,139 +5,73 @@ import java.util.Random;
 
 class Main {
     public static void main(String[] args) {
-        //checkNumber();
-        getArrayWithoutNumber();
-        //getMinMaxAverage();
-        // equalAverage();
+        getTripleArray();
+        //getChessBoard();
+
     }
-
-    //0
-    public static void checkNumber() {
-
-        int[] number = new int[]{4, 6, 78, 12, 48, 9, -7};
-        for (int i = 0; i < number.length; i++) {
-            System.out.println(number[i]);   //вывести массив на экран
-        }
-        System.out.print("Please, enter your number: ");
-        Scanner enter = new Scanner(System.in);
-        int size = enter.nextInt();
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] == size) {
-                System.out.print("There is such a number in the array: ");
-                break;
+    //    Создать трехмерный массив из целых чисел.
+//    С помощью циклов "пройти" по всему массиву и увеличить каждый элемент на
+//    заданное число. Пусть число, на которое будет увеличиваться каждый элемент,
+//    задается из консоли.
+    public static void getTripleArray() {
+        int[][][] cubeNames2 = new int[][][]{
+                {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                }
+        };
+        for (int j = 0; j < cubeNames2.length; j++) {
+            for (int y = 0; y < cubeNames2[j].length; y++) {
+                for (int z = 0; z < cubeNames2[j][y].length; z++) {
+                    System.out.print(cubeNames2[j][y][z] + " - ");
+                }
+                System.out.println();
             }
-            if (i == number.length - 1) {
-                System.out.print("There is no a number in the array: ");
+        }
+            Scanner enter = new Scanner(System.in);
+            System.out.print("Please, enter your number: ");
+            int increment = enter.nextInt();
+            for (int j = 0; j < cubeNames2.length; j++) {
+                for (int y = 0; y < cubeNames2[j].length; y++) {
+                    for (int z = 0; z < cubeNames2[j][y].length; z++) {
+                        cubeNames2[j][y][z] += increment;
+                    }
+                }
             }
-            enter.close();
-        }
-    }
-
-    //1
-    public static void getArrayWithoutNumber () {
-        int[] array = new int[]{5, 8, 19, 63, 54};
-        Scanner enter = new Scanner(System.in);
-        System.out.print("Please, enter your number: "); //число для удаления
-        int number = enter.nextInt();
-        boolean found = false;
-        int newSize = 0;
-        for (int i = 0; i < array.length; i++)
-            if (array[i] == number) {
-                found = true;
-            } else {
-                array[newSize] = array[i];
-                newSize++;
+            System.out.println("New array after change: ");
+            for (int j = 0; j < cubeNames2.length; j++) {
+                for (int y = 0; y < cubeNames2[j].length; y++) {
+                    for (int z = 0; z < cubeNames2[j][y].length; z++) {
+                        System.out.print(cubeNames2[j][y][z] + " - ");
+                    }
+                    {
+                        System.out.println();
+                    }
+                }
+                System.out.println();
             }
-//         Вывод результата
-        if (found) {
-            int[] newArray = new int[newSize]; // новый массив
-            System.arraycopy(array, 0, newArray, 0, newSize);
-            //System.arrayCopy(from, fromIndex, to, toIndex, count);
-//            from - массив, который копируем
-//            to - массив в которой копируем
-//            fromIndex - индекс в массиве from начиная с которого берем элементы для копирования
-//            toIndex - индекс в массиве to начиная с которого вставляем элементы
-//            count - количество элементов которые берем из массива from и вставляем в массив to
-//            Массив to должен иметь достаточный размер, чтобы в нем уместились все копируемые элементы.
-            System.out.println("Элементы массива после удаления:");
+        }
 
-            for (int i = 0; i < newArray.length; i++) {
-                System.out.println(newArray[i]);
+
+//    Создать программу для раскраски шахматной доски с помощью цикла. Создать двумерный массив String'ов 8х8.
+//    С помощью циклов задать элементам циклам значения B(Black) или W(White).  Результат работы программы:
+
+            public static void getChessBoard () {
+                 String[][] array = new String[8][8];
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        if ((i + j) % 2 == 0) array[i][j] = "W";
+                        else array[i][j] = "B";
+
+                        System.out.print(" " + array[i][j]);
+                    }
+                    System.out.println();
+                }
             }
-
-        } else {
-            System.out.println("Число не найдено в массиве.");
-        }
-    }
-
-    //2
-    public static void getMinMaxAverage () {
-        Scanner enter = new Scanner(System.in);
-        System.out.print("Please, enter your  size arrays: ");
-        int size = enter.nextInt();
-
-        int[] mass = new int[size];// создание массива
-        Random random = new Random();// заполнение массива случайными числами
-        for (int i = 0; i < size; i++) {
-            mass[i] = random.nextInt(10); // шагать рандому до кагого числа
-        }
-//            mass[i] = enter.nextInt();
-//        }
-        for (int i = 0; i < size; i++) {
-            System.out.println(i + "-" + mass[i]);
-        }
-
-        int min = mass[0];
-        int max = mass[0];
-        int sum = 0;
-        for (int i = 0; i < size; i++) {
-            if (mass[i] < min) {
-                min = mass[i];
-                //System.out.println("цикл  бежит по мин: " + min); // цикл бежит по мин
-            }
-            if (mass[i] > max) {
-                max = mass[i];
-                //System.out.println("цикл  бежит по макс: " + max); // цикл бежит по макс
-            }
-            sum = sum + mass[i];
-            //System.out.println("цикл  бежит: " + sum); // цикл бежит
-        }
-        double average = (double) sum / size;
-        // Вывод результатов
-        System.out.println("Min: " + min);
-        System.out.println("Max: " + max);
-        System.out.println("Average: " + average);
-        enter.close();
-    }
-
-    //3.
-    public static void equalAverage () {
-        // вывод массива на консоль
-        int[] array1 = new int[]{0, 12, 9, 8, 82};
-        int[] array2 = new int[]{1, 16, 7, 8, 90};
-        System.out.println(Arrays.toString(array1));
-        System.out.println(Arrays.toString(array2));
-        // сравнение средн.арифмитич. и вывод результата
-        int sum = 0;
-        int sum1 = 0;
-        for (int i = 0; i < array1.length; i++) {
-            sum = sum + array1[i];
-        }
-        double average = (double) sum / 5;
-        System.out.println("Arithmetic mean array1: " + average);  //среднее арифмит.зн. sum
-
-        for (int i = 0; i < array2.length; i++) {
-            sum1 = sum1 + array2[i];
-        }
-        double average1 = (double) sum1 / 5;
-        System.out.println("Arithmetic mean array2: " + average1);  //среднее арифмит.зн.sum1
-
-        if (average > average1) {
-            System.out.println("The arithmetic mean of the first array is greater");
-        } else if (average < average1) {
-            System.out.println("The arithmetic mean of the second array is greater");
-        } else {
-            System.out.println("The arithmetic mean equal");
-        }
-    }
 }
+
+
+
+
+
